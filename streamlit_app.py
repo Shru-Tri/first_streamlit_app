55b5c93a79210d1b8406c23cb208ef3e85a40a00
Shruti <43470996+Shru-Tri@users.noreply.github.com>
2022-11-29 10:35:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.header ('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #Adding the csv data from the S3 bucket to be displayed as a dataframe in streamlit library.
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #Let's introduce a widget called multiselect
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
