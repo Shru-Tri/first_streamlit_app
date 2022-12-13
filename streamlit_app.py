9669fa89257f2d5fe2983bc5a4dafa7a04a5e6c0
Shruti <43470996+Shru-Tri@users.noreply.github.com>
2022-12-13 10:36:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,24 +19,3 @@ conn = init_connection()
 
 # SQL query
 
-query = """
-    SELECT 
-        TYPE
-    FROM AUTHOR_INGEST_PARQUET;
-    """
-
-# Perform query.
-# Creating a function to load the data into a pandas data frame
-def load_data():
-    cur = conn.cursor().execute(query)
-    table1_df = pd.DataFrame.from_records(iter(cur), 
-                  columns=[x[0] for x in cur.description])
-    return payments_df
-
-
-#q1_df = pd.DataFrame(rows.items(), columns =['SHOW_ID', 'TYPE', 'TITLE', 'DIRECTOR', 'CAST', 'COUNTRY', 'DATE_ADDED', 'RELEASE_YEAR', 'RATING', 'DURATION', 'LISTED_IN', 'DESCRIPTION'])
-#st.table(q1_df)
-#st.dataframe(q1_df)
-# Print results.
-#for row in rows:
-# st.write(f"{row[0]} has a :{row[1]}:")
