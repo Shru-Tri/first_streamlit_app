3b201f33c17ef2bfa347d587bab48c3fe94f7cd6
Shruti <43470996+Shru-Tri@users.noreply.github.com>
2022-12-13 12:04:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.title('My First Visuals App!')
 #Adding the csv data from the S3 bucket to be displayed as a dataframe in streamlit library.
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 import snowflake.connector
 
@@ -29,4 +30,17 @@ rows = run_query("SELECT * from AUTHOR_INGEST_PARQUET;")
 df = pd.DataFrame(rows, columns = ['SHOW_ID', 'TYPE', 'TITLE', 'DIRECTOR', 'CAST', 'COUNTRY', 'DATE_ADDED', 'RELEASE_YEAR', 'RATING', 'DURATION', 'LISTED_IN', 'DESCRIPTION'])
 table1_df = df.dropna() 
 st.dataframe(table1_df)
-#new_df = table1_df.dropna()
+##################################
+type_count = table1_df['type'].value_counts().to_dict()
+
+type_count
+
+p1 = plt.bar(range(len(type_count)), list(type_count.values()))
+size = len(table1_df)
+for rect1 in p1:
+    height = rect1.get_height()
+    plt.annotate( "{}%".format(round((height*100/size),3)),(rect1.get_x() + rect1.get_width()/2, height+.05),ha="center",va="bottom",fontsize=15)
+
+plt.xticks(range(len(type_count)), list(type_count.keys()))
+plt.title('Movies vs TV Show')
+plt.show()
