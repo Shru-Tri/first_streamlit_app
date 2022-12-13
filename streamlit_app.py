96b160e1748667707aa6f575236a654bdfcb28be
Shruti <43470996+Shru-Tri@users.noreply.github.com>
2022-12-13 12:54:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ rows = run_query("SELECT * from AUTHOR_INGEST_PARQUET;")
 # Print results.
 df = pd.DataFrame(rows, columns = ['SHOW_ID', 'TYPE', 'TITLE', 'DIRECTOR', 'CAST', 'COUNTRY', 'DATE_ADDED', 'RELEASE_YEAR', 'RATING', 'DURATION', 'LISTED_IN', 'DESCRIPTION'])
 table1_df = df.dropna() 
-table1_df['DATE ADDED'] = pd.to_datetime(table1_df['DATE_ADDED'])
+table1_df['DATE ADDED'] = pd.to_datetime(table1_df['DATE_ADDED'], inplace = TRUE)
+
 st.dataframe(table1_df)
 
 ##################################
